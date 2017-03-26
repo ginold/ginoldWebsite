099ef730c55012f0404dd0f50042951cceab9fca
ginold <ginold@hotmail.com>
2017-03-26 21:24:12 +0200

    progress!

--- app/app.aboutMe.ts
+++ app/app.aboutMe.ts
@@ -14,20 +14,20 @@ export class AboutMe {
 	readonly languages: Array<Object>;
 
 	constructor(){
-		this.tabs = [
-			{title: 'Me, myself & I', color: 'red', id: 'myself', icon: 'child'},
-			{title: 'What I\'ve learned', color: 'purple', id: 'education', icon: 'student'},
-			{title: 'Where I\'ve worked', color: 'yellow', id: 'experience', icon: 'bar'},
-			{title: 'What I know', color: 'teal', id: 'competences', icon: 'puzzle'}
-		];
+		// this.tabs = [
+		// 	{title: 'Me, myself & I', color: 'red', id: 'myself', icon: 'child'},
+		// 	{title: 'What I\'ve learned', color: 'purple', id: 'education', icon: 'student'},
+		// 	{title: 'Where I\'ve worked', color: 'yellow', id: 'experience', icon: 'bar'},
+		// 	{title: 'What I know', color: 'teal', id: 'competences', icon: 'puzzle'}
+		// ];
 		this.progLangs = [
-			{title: 'Android', value: 0.35},
-			{title: 'HTML, CSS', value: 0.85},
-			{title: 'Angular', value: 0.90},
-			{title: 'Photoshop', value: 0.5},
-			{title: 'After Effects', value: 0.85},
-			{title: 'Premiere', value: 0.90},
-			{title: 'Fruity Loops', value: 0.5}
+			{title: 'Android', value: 0.35, img: 'android.png'},
+			{title: 'HTML, CSS', value: 0.85, img: 'html.png'},
+			{title: 'Angular', value: 0.90, img: 'angular.png'},
+			{title: 'Photoshop', value: 0.5, img: 'photoshop.svg'},
+			{title: 'After Effects', value: 0.85, img: 'after_effects.svg'},
+			{title: 'Premiere', value: 0.90, img: 'premiere.svg'},
+			{title: 'Fruity Loops', value: 0.5, img: 'fl.jpg'}
 		];
 		this.languages = [
 			{title: 'polish', value: 0.99, country: 'poland', level: 'C2'},
@@ -43,6 +43,37 @@ export class AboutMe {
 		  transition: 'vertical flip in',
 		  duration: 500
 		});
+
+
+	}
+
+	ngAfterViewInit(): void {
+		$('.programming-item').on('mouseenter', function(e){
+			var progressCircle = $(e.currentTarget).find('.progress-circle');
+			var container = $(e.currentTarget).find('.programming-title-container');
+			var fullId = progressCircle.attr('id');
+			var id = parseInt(fullId[fullId.length - 1]);
+			var circleExists = progressCircle.find('.progressbar-text');
+			var value = this.progLangs[id].value;
+
+			if(!circleExists.length) {
+				$(progressCircle).addClass('show-circle');
+				$(container).addClass('show-circle'); // doesn't work if on the same line
+
+				var bar = new ProgressBar.Circle('#' + fullId, {
+				  strokeWidth: 4, easing: 'easeInOut', duration: 1400,
+				  from: { color: '#aaa', width: 1 },
+				  to: { color: '#333', width: 4 },
+				  // Set default step function for all animate calls
+				  step: function(state, circle) {
+				    circle.path.setAttribute('stroke', state.color);
+				    circle.path.setAttribute('stroke-width', state.width);
+				    circle.setText(Math.round(circle.value() * 100); + '%');
+				  }
+				});
+				bar.animate(value); // Number from 0.0 to 1.0
+			}
+		}.bind(this))
 	}
 
 
@@ -50,11 +81,6 @@ export class AboutMe {
 		$('.tab-content, .tab-content.visible.transition').removeClass('visible transition').hide();
 		$('#' + tab).transition('fade left');
 
-		$('#competences .progress-circle').circleProgress({
-			size: 200
-		}).on('circle-animation-progress', function(event:any, progress:any, stepValue:any) {
-			$(this).find('strong').text(stepValue.toFixed(2).substr(2) + '%');
-		});
 
 		$('.fadeUpCircle').transition({
 			animation : 'fade up',
--- app/working/working.ts
+++ app/working/working.ts
@@ -13,12 +13,20 @@ export class WorkingExperience {
 
 	constructor() {
 		this.works = [
-		{ title: 'NagraVision Kudelski', duration: '1.5 years', width: 35, color: 'red'},
-		{ title: 'Okay Ad Agency', duration: '1 month', width: 10, color: 'green'},
-		{ title: 'Mahogany Hall',  duration: '1 month', width: 10, color: 'yellow' },
-		{ title: 'Vélocité sàrl', duration: '4 months', width: 15, color: 'purple'},
-		{ title: 'Camptocamp SA', duration: '1 year', width: 30, color: 'orange'}
+		{ title: 'NagraVision Kudelski', duration: '1.5 years', width: 35, position: 'intern', img: 'nagravision.jpg',
+			'description': 'Cable welding, poster design, learning to code, quality control', 'date': 'December 2010 - June 2012'},
+		{ title: 'Okay Radio Ad Agency', duration: '1 month', width: 10, position: 'intern', img: 'radio_katowice.png',
+			'description': 'Writing ad scenarios', 'date': 'July 2015'},
+		{ title: 'Mahogany Hall',  duration: '1 month', width: 10, position: 'front-end developer', img: 'mahogany_small.png',
+			'description': 'Developing an Event Managment App', 'date': 'May 2015'},
+		{ title: 'Vélocité sàrl', duration: '4 months', width: 15, position: 'full-stack developer', img: 'velocite.png',
+			'description': 'Developing a Schedule Manager for administrators and employees', 'date': 'May - September 2015'},
+		{ title: 'Camptocamp SA', duration: '1 year', width: 30, position: 'front-end developer', img: 'camptocamp.png',
+			'description': 'Developing the new Camptocamp.org website from scratch, EPFL Open Days App', 'date': 'January - December 2016'}
 
 		];
+	}
+	ngAfterViewInit(): void {
+
 	}
 }
